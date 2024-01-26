bcaf0caabccf928ed385b7ba3e1d3bc140733ada
Bhaveshg4 <118620727+Bhaveshg4@users.noreply.github.com>
2024-01-26 15:53:09 +0530

    Login Logic upgraded

--- lib/pages/adminlogin.dart
+++ lib/pages/adminlogin.dart
@@ -21,137 +21,124 @@ class AdminLogin extends StatelessWidget {
           style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
         ),
       ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/background.png'),
-                fit: BoxFit.cover,
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Card(
+            color: Color.fromARGB(0, 233, 227, 227),
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
             ),
-          ),
-          Container(
-            color: Colors.black.withOpacity(0.5),
-          ),
-          Center(
-            child: Card(
-              color: Color.fromARGB(0, 233, 227, 227),
-              elevation: 8.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        alignment: Alignment.center,
-                        height: 40,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.red.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: const Text(
-                          "Admin Login",
-                          style: TextStyle(fontSize: 24, color: Colors.amber),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      height: 40,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.red.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "Admin Login",
+                        style: TextStyle(fontSize: 24, color: Colors.amber),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    TextFormField(
+                      controller: usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
-                      const SizedBox(height: 10.0),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: TextFormField(
-                          controller: usernameController,
-                          decoration: const InputDecoration(
-                            labelText: 'Username',
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your username';
-                            }
-                            return null;
-                          },
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
-                      const SizedBox(height: 16.0),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your password';
-                            }
-                            return null;
-                          },
-                        ),
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
-                      const SizedBox(height: 24.0),
-                      ElevatedButton(
-                        onPressed: () async {
-                          if (formKey.currentState!.validate()) {
-                            String enteredUsername = usernameController.text;
-                            String enteredPassword = passwordController.text;
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 24.0),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          String enteredUsername = usernameController.text;
+                          String enteredPassword = passwordController.text;
 
-                            DocumentSnapshot snapshot = await FirebaseFirestore
-                                .instance
-                                .collection('users')
-                                .doc('credentials')
-                                .get();
+                          DocumentSnapshot snapshot = await FirebaseFirestore
+                              .instance
+                              .collection('users')
+                              .doc('credentials')
+                              .get();
 
-                            String storedUsername = snapshot['username'];
-                            String storedPassword = snapshot['password'];
+                          String storedUsername = snapshot['username'];
+                          String storedPassword = snapshot['password'];
 
-                            if (enteredUsername == storedUsername &&
-                                enteredPassword == storedPassword) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomePage(),
-                                ),
-                              );
-                            } else {
-                              print('Invalid credentials');
-                            }
+                          if (enteredUsername == storedUsername &&
+                              enteredPassword == storedPassword) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                            );
+                          } else {
+                            print('Invalid credentials');
                           }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
